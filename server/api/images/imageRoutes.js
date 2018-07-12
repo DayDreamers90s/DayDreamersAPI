f675e87138f4b7d94abf6a062563d333a7cd94ab
Sri Harsha Jilludumudi <sriharshajillu@outlook.com>
2018-07-11 20:46:42 -0400

    many logs added

--- server/api/images/imageRoutes.js
+++ server/api/images/imageRoutes.js
@@ -1,43 +1,73 @@
 var router = require('express').Router();
 var controller = require('./imageController');
 var UPLOAD_PATH = 'uploads';
-var multer=require('multer');
+var multer = require('multer');
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-      console.log('destination: '+ UPLOAD_PATH);
-      cb(null, UPLOAD_PATH)
-  },
-  filename: function (req, file, cb) {
-      console.log('filename: ' +  file.fieldname + '-' + Date.now());
-      cb(null, file.fieldname + '-' + Date.now())
-  }
+    destination: function (req, file, cb) {
+        console.log('destination: ' + UPLOAD_PATH);
+        console.log('full path: ' + file.path);
+        console.log('mime type: ' + file.mimetype);
+        console.log('buffer: ' + file.buffer);
+        cb(null, UPLOAD_PATH)
+    },
+    filename: function (req, file, cb) {
+        console.log('filename: ' + file.fieldname + '-' + Date.now());
+        cb(null, file.fieldname + '-' + Date.now())
+    }
 })
-var upload = multer({ storage: storage })
+// var upload = multer({ storage: storage })
 
-router.param('id', controller.params);  
+router.param('id', controller.params);
 
 router.route('/')
-.get(controller.get)
+    .get(controller.get)
 // .post(upload.single('image'), controller.post)
-router.post('/', upload.single('image'), (req, res, next) => {
-    console.log('********************');
-    console.log(JSON.stringify(req));
-    // Create a new image model and fill the properties
-    let newImage = new Image();
-    newImage.filename = req.file.filename;
-    newImage.originalName = req.file.originalname;
-    newImage.desc = req.body.desc
-    newImage.save(err => {
+var multerupload = multer({ storage: storage }).single('image');
+
+router.post('/', function (req, res) {
+    multerupload(req, res, function (err) {
         if (err) {
-            return res.sendStatus(400);
+            console.log('********Error in multer************');
+            console.log(err);
+            return res.sendStatus(500).send(err);
         }
-        res.status(201).send({ newImage });
-    });
-});
+    }), (req, res, next) => {
+        console.log('********************');
+        console.log(JSON.stringify(req));
+        // Create a new image model and fill the properties
+        let newImage = new Image();
+        newImage.filename = req.file.filename;
+        newImage.originalName = req.file.originalname;
+        newImage.desc = req.body.desc;
+        newImage.save(err => {
+            if (err) {
+                return res.sendStatus(400);
+            }
+            res.status(201).send({ newImage });
+        })
+    }
+})
+
+
+// router.post('/', upload.single('image'), (req, res, next) => {
+//     console.log('********************');
+//     console.log(JSON.stringify(req));
+//     // Create a new image model and fill the properties
+//     let newImage = new Image();
+//     newImage.filename = req.file.filename;
+//     newImage.originalName = req.file.originalname;
+//     newImage.desc = req.body.desc
+//     newImage.save(err => {
+//         if (err) {
+//             return res.sendStatus(400);
+//         }
+//         res.status(201).send({ newImage });
+//     });
+// });
 
 router.route('/:id')
-  .get(controller.getOne)
-//   .put(controller.put)
-  .delete(controller.delete)
+    .get(controller.getOne)
+    //   .put(controller.put)
+    .delete(controller.delete)
 
 module.exports = router;
